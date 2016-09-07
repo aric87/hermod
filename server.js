193569773f3d0ec423c657954bf1143280276682
Aric Johnson <aric87@comcast.net>
2016-09-07 00:41:47 -0400

    nit

---
+++ server.js
@@ -0,0 +1,62 @@
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var app_ip_address = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var bodyParser   = require('body-parser');
+var cors = require('cors');
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+var wellknown = require('nodemailer-wellknown');
+var morgan = require('morgan');
+var whitelist = ['http://example1.com', 'http://example2.com'];
+var corsOptions = {
+  origin: function(origin, callback){
+    var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+    callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted);
+  }
+};
+app.options('*', cors());
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // get information from html forms
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.post('/email', cors(), function(req, res) {
+  var {sendTo, subject, text} = req.body;
+  if (!sendTo || !subject || !text){
+    var emessage = 'You\'re missing something';
+    if (!sendTo){
+      emessage+= 'You need a recieving address.'
+    }
+    if (!subject)
+    return res.status(400).json({message:`You're missing somet`})
+  }
+  var smtpTransport = nodemailer.createTransport({
+      service: process.env.SERVICE,
+      auth: {
+              user: process.env.EMAIL,
+              pass: process.env.PASS
+          }
+  }),
+  mailOptions = {
+      to: sendTo,
+      from: process.env.EMAIL,
+      subject: subject,
+      text: text
+  };
+  smtpTransport.sendMail(mailOptions, function (err) {
+    if (err){
+      return res.status(404).json({message:'error: \n'+ err})
+
+    }
+    return res.status(200).json({message:'The email was sent!'})
+  });
+});
+
+app.listen(port,app_ip_address);
+console.log('The magic happens on port ' + port);
+
+module.exports  = app;
